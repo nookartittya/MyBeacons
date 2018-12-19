cf46a29312be0946cb72c4b28a8c4c779339903a
nookartittya <44186182+nookartittya@users.noreply.github.com>
2018-12-19 11:12:27 +0700

    Initial commit

---
+++ app/src/main/java/com/estimote/proximity/MyApplication.java
@@ -0,0 +1,15 @@
+package com.estimote.proximity;
+
+import android.app.Application;
+
+import com.estimote.proximity_sdk.api.EstimoteCloudCredentials;
+
+//
+// Running into any issues? Drop us an email to: contact@estimote.com
+//
+
+public class MyApplication extends Application {
+
+    public EstimoteCloudCredentials cloudCredentials =
+            new EstimoteCloudCredentials("proximity-for-a-single-bea-38x", "88cceff6e8ad79cccace067729c5ca7d");
+}
